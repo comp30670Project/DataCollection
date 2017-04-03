e8531ee58948b7054cd820159857306b429936a3
ojhermann-ucd <otto.hermann@ucdconnect.ie>
2017-04-03 23:42:05 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+from os import name
+
+setup(
+    name = "comp30670Assignment4 data collection",
+    description = "method for collecting data for assignment4",
+    author = "Otto Hermann",
+    author_email = "otto.hermann@ucdconnect.ie",
+    url="https://github.com/comp30670Project/DataCollection.git",
+    packages = ['JSON', 'SQL'],
+    )
